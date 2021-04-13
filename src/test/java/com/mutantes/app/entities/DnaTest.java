52add23cb17ec603fd93ce1d389cda8f8adff9c0
daahernandezca <daahernandezca@gmail.com>
2021-04-13 02:09:37 -0500

    delete dependencies

--- src/test/java/com/mutantes/app/entities/DnaTest.java
+++ src/test/java/com/mutantes/app/entities/DnaTest.java
@@ -1,17 +1,11 @@
 package com.mutantes.app.entities;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
 
 public class DnaTest {
